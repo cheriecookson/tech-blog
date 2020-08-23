4c8a1c0d3777810ce955854693c4fb1515a1bf1c
Cherie Cookson <cheriecookson@gmail.com>
2020-08-23 17:11:08 -0500

    adding stuss

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,63 @@
+const router = require('express').Router();
+// const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+router.post('/', withAuth, (req, res) => {
+    const body = req.body;
+   Post.create({
+    ...body, user_id: req.session.user_id
+  })
+    .then(dbPostData => {
+        res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+  Post.update(req.body, 
+      {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+  console.log('id', req.params.id);
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
